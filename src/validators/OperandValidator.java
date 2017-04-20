4d34fef0313fabc9af64f12d868ece7bb50acee7
MuhammedKhamis <muhammedessam10@gmail.com>
2017-04-21 00:16:37 +0200

    ss

--- src/validators/OperandValidator.java
+++ src/validators/OperandValidator.java
@@ -1,7 +1,22 @@
 package validators;
 
-import statement.Directive;
 import statement.IStatement;
+import statement.Operation;
+
+//instruction 12344 hexadexcimal address
+// instruction #12344 decimal value
+// instruction label label address
+// instruction #label  address
+// instruction @label  address
+// instruction label,x address
+//**********************************************
+//word -(4 decimal digits) or 4 decimal digits
+//byte C'max 15 char'
+//byte X'max 14 hexa digits'
+// REWS RESW  operand max 4 decimal digits
+// directive 123 decimal value  -->   word,resw,resb
+// directive X'123' hexadecimal value --> byte
+// directive C'123' string value --> byte
 
 public class OperandValidator implements IValidator {
 
@@ -12,16 +27,45 @@ public class OperandValidator implements IValidator {
     @Override
     public Boolean validate(String content, IStatement operation) {
         String str = content.trim();
-        switch (operation.getFormatType()) {
-            case 1:
-                return format1(str);
-            case 2:
-                return format2(str);
-            case 3:
-                return format3(str, operation);
-            case 4:
-                return format4(str, operation);
+        if (operation instanceof Operation) {
+            switch (operation.getFormatType()) {
+                case 1:
+                    return format1(str);
+                case 2:
+                    return format2(str);
+                case 3:
+                    return format3(str, operation);
+                case 4:
+                    return format4(str, operation);
+            }
+        } else {
+            return checkDirective(str);
         }
+        return false;
+    }
+
+    private boolean checkDirective(String content) {
+        if (content.charAt(0) == 'C' || content.charAt(0) == 'c') {
+            if (content.charAt(1) == '\'' && content.charAt(content.length() - 1) == '\'')
+                if (content.substring(1, content.length() - 1).length() <= 15)
+                    return checkChar(content.substring(2, content.length() - 1));
+        }
+        if (content.charAt(0) == 'X' || content.charAt(0) == 'x') {
+            if (content.charAt(1) == '\'' && content.charAt(content.length() - 1) == '\'')
+                return checkHexaNumber(content.substring(2, content.length() - 1))
+                        && content.substring(1, content.length() - 1).length() <= 14;
+        }
+        if (content.length() <= 5) {
+            if (content.charAt(0) == '-') {
+                return checkDecimalNumber(content.substring(1));
+            } else {
+                return checkDecimalNumber(content);
+            }
+        }
+        return false;
+    }
+
+    private boolean checkChar(String content) {
         return false;
     }
 
@@ -40,25 +84,25 @@ public class OperandValidator implements IValidator {
     }
 
     private boolean format3(String content, IStatement operation) {
-        if (operation instanceof Directive) {
-            return checkDecimalNumber(content);
-        } else {
-            if (content.charAt(0) == '#' || content.charAt(0) == '@') {
-                return checkName(content.substring(1)) || checkDecimalNumber(content.substring(1));
-            }
-            if (content.substring(0, 2).equalsIgnoreCase("0x")) {
-                return checkHexaNumber(content.substring(3));
-            }
-            return checkName(content);
+        if (content.substring(0, 2).equalsIgnoreCase("0x")) {
+            return checkHexaNumber(content.substring(3));
         }
+        return generalChecker(content, operation);
+    }
+
+    private boolean generalChecker(String content, IStatement operation) {
+        if (content.charAt(0) == '#' || content.charAt(0) == '@') {
+            return checkName(content.substring(1)) || checkDecimalNumber(content.substring(1));
+        }
+        return checkName(content);
 
     }
 
     private boolean format4(String content, IStatement operation) {
-        if (operation instanceof Directive) {
-            return false;
+        if (content.substring(0, 2).equalsIgnoreCase("0x")) {
+            return checkHexaNumber(content.substring(3));
         }
-        return format3(content, operation);
+        return generalChecker(content, operation);
     }
 
     private boolean isRegister(String reg) {
@@ -98,7 +142,6 @@ public class OperandValidator implements IValidator {
     }
 
     private boolean checkHexaNumber(String content) {
-
         for (int i = 0; i < content.length(); i++) {
             if (inBetween(content, i, '0', '9'))
                 continue;
