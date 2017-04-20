1eb80b5f47cf149f30ad6af4c182429041eca9b8
MuhammedKhamis <muhammedessam10@gmail.com>
2017-04-20 22:52:04 +0200

    Operand Validator , Adding Pair , modifiing line Parser

--- src/parsers/LineParser.java
+++ src/parsers/LineParser.java
@@ -1,43 +1,58 @@
 package parsers;
 
+import util.Pair;
+
 public class LineParser {
-	
-	private static LineParser parserObj;
-	private LineParser(){
-	}
-	
-	public String[] parseLine(String line){
-		if(validateLineFormat(line)){
-			return extractParameters(line);
-		}
-		return null;
-	}
-	
-	//validate line format length range only
-	private boolean validateLineFormat(String line){
-		while(line.length()<66){
-			line+=" ";
-		}
-		boolean cond1 = (line.length() < 67);
-		boolean cond2 = (line.substring(0, 8).split(" +").length <= 1);
-		boolean cond3 = (line.substring(9, 14).split(" +").length == 1);
-		boolean cond4 = (line.substring(17, 34).split(" +").length <= 1);
-		boolean cond5 = (line.charAt(8)== ' ');
-		boolean cond6 = line.substring(15, 17).equals("  ");
-		return cond1&&cond2&&cond3&&cond4&&cond5&&cond6;
-	}
-	
-	//extract line content [label,operation name,operands,comment]
-	private String[] extractParameters(String line){
-		//{ , , , } if no operand return spaces
-		return line.split(" +");
-	}
-	
-	//signelton
-	public static LineParser getInstance(){
-		if(parserObj==null){
-			parserObj = new LineParser();
-		}
-		return parserObj;
-	}
+
+    private static LineParser parserObj;
+    private static final int numberOfParameters = 4;
+    private static final Pair[] places = new Pair[numberOfParameters];
+
+    private LineParser() {
+        places[0] = new Pair(0, 9);
+        places[1] = new Pair(10, 16);
+        places[2] = new Pair(18, 36);
+        places[3] = new Pair(36, 67);
+    }
+
+    public String[] parseLine(String line) {
+        if (validateLineFormat(line)) {
+            return extractParameters(line);
+        }
+        return null;
+    }
+
+    // validate line format length range only
+    private boolean validateLineFormat(String line) {
+        while (line.length() < 66) {
+            line += " ";
+        }
+        boolean cond1 = (line.length() < 67);
+        boolean cond2 = (line.substring(0, 8).split(" +").length <= 1);
+        boolean cond3 = (line.substring(9, 14).split(" +").length == 1);
+        boolean cond4 = (line.substring(17, 34).split(" +").length <= 1);
+        boolean cond5 = (line.charAt(8) == ' ');
+        boolean cond6 = line.substring(15, 17).equals("  ");
+        return cond1 && cond2 && cond3 && cond4 && cond5 && cond6;
+    }
+
+    // extract line content [label,operation name,operands,comment]
+    private String[] extractParameters(String line) {
+        while (line.length() < 66) {
+            line += " ";
+        }
+        String[] parameters = new String[numberOfParameters];
+        for (int i = 0; i < numberOfParameters; i++) {
+            parameters[i] = line.substring(places[i].getFirst(), places[i].getSecond());
+        }
+        return parameters;
+    }
+
+    // signelton
+    public static LineParser getInstance() {
+        if (parserObj == null) {
+            parserObj = new LineParser();
+        }
+        return parserObj;
+    }
 }
--- src/validators/OperandValidator.java
+++ src/validators/OperandValidator.java
@@ -2,14 +2,97 @@ package validators;
 
 import statement.IStatement;
 
-public class OperandValidator implements IValidator{
+public class OperandValidator implements IValidator {
 
-	public OperandValidator() {
+    public OperandValidator() {
 
-	}
-	
-	@Override
-	public Boolean validate(String content, IStatement operation) {
-		return true;
-	}
+    }
+
+    @Override
+    public Boolean validate(String content, IStatement operation) {
+        String str = content.trim();
+        switch (operation.getFormatType()) {
+            case 1:
+                return format1(str);
+            case 2:
+                return format2(str);
+            case 3:
+                return format3(str);
+            case 4:
+                return format4(str);
+        }
+        return false;
+    }
+
+    private boolean format1(String content) {
+        return content.isEmpty();
+    }
+
+    private boolean format2(String content) {
+        if (content.length() == 1) {
+            return isRegister(content);
+        }
+        if (content.length() == 3) {
+            return isRegister(content.substring(0, 1)) && isRegister(content.substring(2));
+        }
+        return false;
+    }
+
+    private boolean format3(String content) {
+        if (content.charAt(0) == '#' || content.charAt(0) == '@') {
+            return checkName(content.substring(1));
+        }
+        if (content.substring(0, 2).equalsIgnoreCase("0x")) {
+            return checkNumber(content.substring(3));
+        }
+        return checkName(content);
+    }
+
+    private boolean format4(String content) {
+        return format3(content);
+    }
+
+    private boolean isRegister(String reg) {
+        String[] registers = new String[] {"A", "S", "T", "L", "B", "F"};
+        for (String c : registers) {
+            if (reg.equalsIgnoreCase(c))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean checkName(String content) {
+        int i = 0;
+        if (inBetween(content, 0, '0', '9'))
+            return false;
+        while (i < content.length()) {
+            if (content.charAt(i) == ',')
+                break;
+            i++;
+        }
+        if (i + 2 == content.length()) {
+            return (content.charAt(i + 1) == 'X' || content.charAt(i + 1) == 'x');
+        }
+        if (i == content.length())
+            return true;
+
+        return false;
+    }
+
+    private boolean checkNumber(String content) {
+        for (int i = 0; i < content.length(); i++) {
+            if (inBetween(content, i, '0', '9'))
+                continue;
+            if (inBetween(content, i, 'A', 'F'))
+                continue;
+            if (inBetween(content, i, 'a', 'f'))
+                continue;
+            return false;
+        }
+        return true;
+    }
+
+    private boolean inBetween(String content, int index, char start, char end) {
+        return content.charAt(index) >= start && content.charAt(index) <= end;
+    }
 }
