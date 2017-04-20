c34a1aa4b14d7824b78697a503009932e794ec7a
Amr Hendy <amr.m.hendy@gmail.com>
2017-04-20 21:35:08 +0200

    2nd commit

--- Assembler/src/statement/IStatement.java
+++
@@ -1,9 +0,0 @@
-package statement;
-
-public interface IStatement {
-	public String getOpName();
-	public String getOpCode();
-	public int getNumberOfOperands();
-	public int getFormatType();
-	public int hasLabel();
-}
--- Assembler/src/validators/IValidator.java
+++
@@ -1,8 +0,0 @@
-package validators;
-
-import statement.IStatement;
-import statement.Operation;
-
-public interface IValidator {
-	Boolean validate(String content, IStatement operation);
-}
